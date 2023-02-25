45d45e41fdbee3c5cda92fdd512fdedcab4d22fd
Rahil <hi.rahilkhan@gmail.com>
2023-02-25 23:57:12 +0530

    api call

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { ApiService } from 'src/app/Service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit   {
+
+  public productList : any ;
+  constructor (private api : ApiService ) { } 
+
+  ngOnInit(): void{
+     this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;   
+    })  }
 
 }
