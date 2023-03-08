768db3ced012254c9b05d9e67804f9323b6ac2b5
Rahil Khan <hi.rahilkhan@gmail.com>
2023-03-09 01:47:59 +0530

    Add products to cart.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,9 +15,81 @@ export class HomeComponent implements OnInit   {
   constructor (private api : ApiService ) { } 
 
   ngOnInit(): void{
-     this.api.getProduct()
-    .subscribe(res=>{
-      this.productList = res;   
-    })  }
+    //  this.api.getProduct()
+    // .subscribe(res=>{
+    //   this.productList = res;   
+    // })  }
+  }
+  productArray =[
+    {
+    proId:1,
+    img:"./assets/img/blackcooker.jpeg",
+    name:"Diamond",
+    amt: 400,
+    qnt:1
+},
+{
+  proId:2,
+  img:"./assets/img/blackcooker.jpeg",
+  name:"Diamond Plain",
+  amt: 800,
+  qnt:1
+},{
+  proId:3,
+  img:"./assets/img/blackcooker.jpeg",
+  name:"Contura",
+  amt: 1000,
+  qnt:1
+},{
+  proId:4,
+  img:"./assets/img/blackcooker.jpeg",
+  name:"Kadai",
+  amt: 1100,
+  qnt:1
+}
+];
+// Increment/Dec
+inc(item: any){
+  // console.log(item.qnt);
+  if(item.qnt != 5){
+  item.qnt += 1;
+ }}
+ 
+dec(item: any){
+  //console.log(item.qnt);
+  if(item.qnt != 1){
+  item.qnt -= 1;
+ }
+}
 
+ 
+ itemsCart:any = [];
+ addCart(category: { proId: any; qnt: any; }){
+  let cartDataNull = 
+  localStorage.getItem('localCart');
+  if(cartDataNull == null){
+    let storeDataGet:any =[];
+    storeDataGet.push(category);
+    localStorage.setItem('localCart',JSON.stringify(storeDataGet));
+  }
+  else {
+    var id = category.proId;
+    let index:number = -1 ;
+    this.itemsCart = JSON.stringify(localStorage.getItem('localCart'));
+    for(let i=0; i<this.itemsCart.length; i++){
+    if(parseInt(id) === parseInt(this.itemsCart[i].proId)){
+      this.itemsCart[i].qnt = category.qnt;
+      index = i;
+      break;
+    }
+  }
+  if(index == -1){
+    this.itemsCart.push(category);
+    localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+  }
+  else{
+    localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+   }
+  }
+ }
 }
