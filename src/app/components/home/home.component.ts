cd1b7c6af756960aeb54be8c6bde05ea3ccaa8d5
Rahil Khan <hi.rahilkhan@gmail.com>
2023-04-16 02:41:06 +0530

    worked on add to cart method

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component , OnInit } from '@angular/core';
-import { ApiService } from 'src/app/Service/api.service';
+// import { ApiService } from 'src/app/Service/api.service';
+import { CartService } from 'src/app/Service/cart.service';
 import { MessengerService } from 'src/app/services/messenger.service';
-// import { Product } from 'src/app/models/product';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,16 +15,56 @@ export class HomeComponent implements OnInit   {
 
   public productList : any ;
   auth: any;
-  constructor (private msg : MessengerService ) { } 
+  api: any;
+  constructor (private msg : MessengerService,private cartservice:CartService ) { } 
 
   ngOnInit(): void{
     //  this.api.getProduct()
     // .subscribe(res=>{
     //   this.productList = res;   
     // })  }
+    this.productArray =[
+      {
+      proId:1,
+      headname:"Plain Cooker ",
+      img:"./assets/img/cooker1.jpg",
+      name:"Diamond",
+      amt: 400.00,
+      qnt:1
+  },
+  {
+    proId:2,
+    headname:"Black Cooker Models",
+    img:"./assets/img/blackcooker.jpeg",
+    name:"Diamond Black",
+    amt: 800.00,
+    qnt:1
+  },{
+    proId:3,
+    headname:"Contura Cooker Models",
+    img:"./assets/img/contura.webp",
+    name:"Contura",
+    amt: 1000,
+    qnt:1
+  },{
+    proId:4,
+    headname:"Kadai Models",
+    img:"./assets/img/kadai.jpg",
+    name:"Kadai",
+    amt: 1100,
+    qnt:1
   }
-  handleAddToCart(){
-    this.msg.sendMsg(this.productArray)
+  ];
+  
+  }
+
+  addToCart(productArray){
+    // this.msg.sendMsg(this.productArray)
+    // this.productArray.forEach((a:any)=>{
+    //   Object.assign(a,{_quantity:1,_total:a.price})
+    // })
+    this..push(this.productArray);
+    alert('Product added to cart!');
   }
   productArray =[
     {
@@ -57,6 +98,7 @@ export class HomeComponent implements OnInit   {
   qnt:1
 }
 ];
+
 // Increment/Dec
 inc(item: any){
   // console.log(item.qnt);
@@ -72,53 +114,56 @@ dec(item: any){
  
 }
 
-itemsCart:any = [];
-addCart(category: any = []){
-  let cartDataNull = 
-localStorage.getItem('localCart');
-if(cartDataNull == null){
-  let storeDataGet:any = [];
-storeDataGet.push(category);
-  localStorage.setItem
-    ('localCart', 
- JSON.stringify(storeDataGet));
-}
-else{
-  var id = category.proId;
-  let index:number = -1;
-  this.itemsCart = 
-JSON.stringify
-(localStorage.getItem
-('localCart'));
-for
-(let i=0; i<this.itemsCart.length; 
-i++){
-if(parseInt(id) === 
-parseInt(this.itemsCart[i].proId))
-{
-    this.itemsCart[i].qnt = 
-category.qnt;
-      index = i;
-      break;
-    }
-  }
-  if(index == -1){
-this.itemsCart.push(category);
-    localStorage.setItem
-('localCart', JSON.stringify
-(this.itemsCart));
-  }
-  else{
-    localStorage.setItem
-('localCart', JSON.stringify
-(this.itemsCart));
-  }
-}
-}cartNumber:number = 0;
-cartNumberFunc(){
-  var cartValue = JSON.stringify (localStorage.getItem('localCart'));
-  this.cartNumber = cartValue.length;
-  this.auth.cartSubject.next(this.cartNumber);
+// itemsCart:any = [];
+// addCart(category: any = []){
+//   let cartDataNull = 
+// localStorage.getItem('localCart');
+// if(cartDataNull == null){
+//   let storeDataGet:any = [];
+// storeDataGet.push(category);
+//   localStorage.setItem
+//     ('localCart', 
+//  JSON.stringify(storeDataGet));
+// }
+// else{
+//   var id = category.proId;
+//   let index:number = -1;
+//   this.itemsCart = 
+// JSON.stringify
+// (localStorage.getItem
+// ('localCart'));
+// for
+// (let i=0; i<this.itemsCart.length; 
+// i++){
+// if(parseInt(id) === 
+// parseInt(this.itemsCart[i].proId))
+// {
+//     this.itemsCart[i].qnt = 
+// category.qnt;
+//       index = i;
+//       break;
+//     }
+//   }
+//   if(index == -1){
+// this.itemsCart.push(category);
+//     localStorage.setItem
+// ('localCart', JSON.stringify
+// (this.itemsCart));
+//   }
+//   else{
+//     localStorage.setItem
+// ('localCart', JSON.stringify
+// (this.itemsCart));
+//   }
+// }
+// }cartNumber:number = 0;
+// cartNumberFunc(){
+//   var cartValue = JSON.stringify (localStorage.getItem('localCart'));
+//   this.cartNumber = cartValue.length;
+//   this.auth.cartSubject.next(this.cartNumber);
+// }
+addtoCart(item:any){
+  this.cartservice.addtoCart(item);
 }
 }
 
